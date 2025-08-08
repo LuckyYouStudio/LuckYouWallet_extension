f1343d311be50886bf1b558ddc121efb39441736
liushuang <54167184+ls19920505@users.noreply.github.com>
2025-08-07 23:20:35 -0700

    Add React popup with web3 wallet features

---
+++ src/manifest.ts
@@ -0,0 +1,10 @@
+export default {
+  manifest_version: 3,
+  name: 'LuckYou Wallet',
+  version: '0.1.0',
+  action: {
+    default_popup: 'popup.html',
+    default_title: 'LuckYou Wallet'
+  },
+  permissions: []
+};
