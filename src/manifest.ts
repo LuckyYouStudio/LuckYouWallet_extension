d17b416634e30c1c67ed3791ee98bb42e8be8903
Shuang Liu <liushuang19920505@gmail.com>
2025-08-10 04:52:18 -0700

    icon

--- src/manifest.ts
+++ src/manifest.ts
@@ -2,6 +2,13 @@ export default {
   manifest_version: 3,
   name: 'LuckYou Wallet',
   version: '0.1.0',
+  icons:{
+    16: 'icons/icon16.png',
+    32: 'icons/icon32.png',
+    48: 'icons/icon48.png',
+    64: 'icons/icon64.png',
+    128: 'icons/icon128.png' 
+  },
   action: {
     default_popup: 'popup.html',
     default_title: 'LuckYou Wallet'
